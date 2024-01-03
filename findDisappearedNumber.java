17f8edc813e4563736277a3bb68de3c8c0cf348f
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2024-01-03 12:38:28 -0500

    Update findDisappearedNumber.java

--- findDisappearedNumber.java
+++ findDisappearedNumber.java
@@ -2,6 +2,7 @@
 //Time complexity- O(N*N)
 //Space Complexity- O(1)
 
+//Optimized Approach: We mark the presence of each number by negating the value at the corresponding index in the array.
 // Time Complexity : O(n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
@@ -12,8 +13,11 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         
+
         List<Integer> result = new ArrayList<>();
         int n = nums.length;
+        
+        //if the value at 'idx' is positive, it means value present at the number(idx+1) is present and negates the value of current element
         for(int i=0;i<nums.length;i++){
                 int idx = Math.abs(nums[i]) - 1;
                 if(nums[idx]>0){
